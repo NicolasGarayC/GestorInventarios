170d96454d0fa971ac28bb3c946a64e48cdb083f
LauraRiano143 <lmriano41@ucatolica.edu.co>
2023-11-20 20:21:26 -0500

    adicion dependencias testing

---
+++ src/test/java/com/project/Project/project/service/CategoriaServiceTest.java
@@ -0,0 +1,20 @@
+package com.project.Project.project.service;
+
+import com.project.Project.project.repository.CategoriaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.project.Project.project.service.CategoriaService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoriaServiceTest {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @Test
+    void createCategoria() {
+
+
+    }
+}
